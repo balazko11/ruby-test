2016d95b9ab9057fead811d81a2420e339cc0e72
Branislav Balaz <branislav.balaz@xit.camp>
2025-02-11 01:15:52 +0100

    with finished printing

--- module-sro-assignment.rb
+++ module-sro-assignment.rb
@@ -31,7 +31,7 @@ def process_entry(entry, duplicates_hash_map)
   value = entry[1]
   file_set = Set.new
   for i in 0..value.length - 1
-    for j in (i+1)..value.length - 1
+    for j in (i + 1)..value.length - 1
       first_file = value[i]
       second_file = value[j]
       if File.size(first_file) == File.size(second_file) && FileUtils.identical?(first_file, second_file)
@@ -55,4 +55,13 @@ initial_file_hash_map = create_file_hash_map(ARGV[0], ARGV[1])
 filtered_file_hash_map = initial_file_hash_map.select { |key, value| value.length > 1 }
 duplicates_hash_map = find_duplicate_files(filtered_file_hash_map)
 
-puts duplicates_hash_map
+if duplicates_hash_map.empty?
+  puts "No duplicates found."
+else
+  puts "Grouped Duplicates:"
+  duplicates_hash_map.each do |hash, files|
+    puts "Hash: #{hash}"
+    puts "Files:"
+    files.each { |file| puts "  #{file}" }
+  end
+end
